5a02f874525d2165e1cd45d90b2fbc5d48b3f582
Jose Gabriel Tamura L <jg.tamura10@uniandes.edu.co>
2017-05-23 13:24:09 -0500

    accounts config. ask user for permission

--- imports/api/Accounts_config.js
+++ imports/api/Accounts_config.js
@@ -1,11 +1,17 @@
 import { Accounts } from 'meteor/accounts-base';
 
-
-
-Accounts.ui.config(
-  {
-    requestPermissions:
-    {
-      google:['https://www.googleapis.com/auth/calendar']
-    }, forceApprovalPrompt: {google: true}
-  });
+Accounts.ui.config({
+    requestPermissions: {
+        google:
+        ['https://www.googleapis.com/auth/calendar',
+        'https://www.googleapis.com/auth/calendar.readonly',
+        'https://www.googleapis.com/auth/userinfo.profile',
+        'https://www.googleapis.com/auth/userinfo.email',
+        'https://www.googleapis.com/auth/tasks'],
+//      forceApprovalPrompt: {google: true}
+    },
+    forceApprovalPrompt: {google: true},
+    requestOfflineToken: {google: true},
+    passwordSignupFields: 'EMAIL_ONLY',
+//      extraSignupFields: []
+});
