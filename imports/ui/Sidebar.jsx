9d2900f8400f5bc0b1d6ff7db522c607ef0267b4
Margarita Gómez <mm.gomez10@uniandes.edu.co>
2017-05-24 16:40:10 -0500

    sidebar changes

--- imports/ui/Sidebar.jsx
+++ imports/ui/Sidebar.jsx
@@ -7,21 +7,23 @@ import SvgIcon from 'react-icons-kit';
 import { ic_aspect_ratio } from 'react-icons-kit/md/ic_aspect_ratio';
 import { ic_business } from 'react-icons-kit/md/ic_business';
 
-export default class Sidebar extends Component{
-render(){
-  return(
-    <div style={{background: '#2c3e50', color: '#FFF', width: 220}}>
-        <SideNav highlightColor='#E91E63' highlightBgColor='#00bcd4' defaultSelected='sales'>
-            <Nav id='dashboard'>
-                <NavIcon><SvgIcon size={20} icon={ic_aspect_ratio}/></NavIcon>
-                <NavText> Dashboard </NavText>
-            </Nav>
-            <Nav id='sales'>
-                <NavIcon><SvgIcon size={20} icon={ic_business}/></NavIcon>
-                <NavText> Sales </NavText>
-            </Nav>
-        </SideNav>
-    </div>
-  );
-}
+export default class SideBar extends Component{
+  render(){
+    return(
+      <div style={{background: '#2c3e50', color: '#FFF', width: 220}}>
+        
+          <h1>holaaa</h1>
+          <SideNav highlightColor='#E91E63' highlightBgColor='#00bcd4' defaultSelected='sales'>
+              <Nav id='dashboard'>
+                  <NavIcon><SvgIcon size={20} icon={ic_aspect_ratio}/></NavIcon>
+                  <NavText> Dashboard </NavText>
+              </Nav>
+              <Nav id='sales'>
+                  <NavIcon><SvgIcon size={20} icon={ic_business}/></NavIcon>
+                  <NavText> Sales </NavText>
+              </Nav>
+          </SideNav>
+      </div>
+    );
+  }
 }
