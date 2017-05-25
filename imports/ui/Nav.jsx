3cb10b1a62e55585b7fea3439399c9276bb8c3ef
Margarita Gómez <mm.gomez10@uniandes.edu.co>
2017-05-25 13:41:05 -0500

    navbar changes

--- imports/ui/Nav.jsx
+++ imports/ui/Nav.jsx
@@ -13,13 +13,17 @@ export default class Nav extends React.Component {
       <div className="navigbar head" >
          <nav className="navbar navbar-default">
         <div className="container-fluid">
-          <div className="nav-things"> 
-            <img className="logo" src= 'NoBG.png' />
-            <ul className="nav navbar-nav navbar-right">
-              <li className="login" >
-                <AccountsUIWrapper />
-              </li>
-            </ul>
+          <div className="nav-things">
+            <div className="col-md-4">
+              <img className="logo" src= 'NoBG.png' />
+            </div>
+            <div className="col-md-8">
+              <ul className="nav navbar-nav navbar-right">
+                <li className="login" >
+                  <AccountsUIWrapper />
+                </li>
+              </ul>
+            </div>
           </div>
         </div>
       </nav>
