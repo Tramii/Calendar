554264d1ecc57293eadf2b69aef8e48ac4d46936
Margarita Gómez <mm.gomez10@uniandes.edu.co>
2017-05-25 13:13:02 -0500

    new calendar

--- imports/ui/Calendar.jsx
+++ imports/ui/Calendar.jsx
@@ -3,74 +3,26 @@ import ReactDOM from 'react-dom';
 
 import { FullCalendar } from 'meteor/jss:fullcalendar-react';
 
-export default class FullcalendarTest extends React.Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      events: [
-        {
-          title: 'Default event',
-          start: new Date(),
-          // plus 30 minutes
-          end: new Date(Date.now + 30 * 60 * 1000),
-        }
-      ],
-    };
-
-    this.onEventSelect = this.onEventSelect.bind(this);
-  }
-
-  onEventSelect(start, end) {
-    const events = this.state.events;
-
-    const newEventsSource = events.concat({
-      title: `Event #${events.length}`,
-      // moment object to simple date object
-      start: start.toDate(),
-      end: end.toDate(),
-    });
-
-    this.setState({
-      events: newEventsSource,
-    });
-  }
-
+export default class Calendar extends React.Component {
   render() {
-    const calendarOptions = {
-      schedulerLicenseKey: 'CC-Attribution-NonCommercial-NoDerivatives',
-
-      header: false,
-
-      id: 'calendar-example',
-      defaultView: 'agendaDay',
-      defaultDate: new Date(),
-      timezone: 'local',
-
-      editable: true,
-      droppable: true,
-      selectable: true,
-
-      slotDuration: '00:15',
-      scrollTime: '08:00',
-      columnFormat: 'ddd DD/MM',
-      displayTime: true,
-      firstDay: 1,
-
-      select: this.onEventSelect,
-
-      // please, use funciton events source for reactivity support
-      events: (start, end, timezone, callback) => {
-        callback(this.state.events);
-      },
-    }
-
-    return(
-      <div className="row">
-        <div className="calendar">
-            <FullCalendar options={calendarOptions} />
-        </div>
-      </div>
-    );
+    return <div id="calendar"></div>;
+  }
+  componentDidMount() {
+    $('#calendar').fullCalendar({
+			header: {
+				left: 'prev,next today',
+				center: 'title',
+				right: 'month,agendaWeek,agendaDay'
+			},
+			editable: true,
+			droppable: true, // this allows things to be dropped onto the calendar
+			drop: function() {
+				// is the "remove after drop" checkbox checked?
+				if ($('#drop-remove').is(':checked')) {
+					// if so, remove the element from the "Draggable Events" list
+					$(this).remove();
+				}
+			}
+    })
   }
 }
